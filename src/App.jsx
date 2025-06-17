bf14c80fd84fd07bb07b7852d5b47f30fd48a9fc
Adithya S Sekhar <adithya.s@edstem.com>
2025-06-17 14:16:16 +0530

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fieldName, setFieldName] = useState("")
+  const [formId, setFormId] = useState("")
+  const [moduleId, setModuleId] = useState("")
+  const [placeholder, setPlaceholder] = useState("")
+  const [tooltip, setTooltip] = useState("")
+
+  const genSchema = {
+    "formFieldId": "b8478f18-478b-4fc0-a4ba-36efbeb949ee",
+    "formId": "e2bacd72-2cc5-4c8c-b2be-ab28631a4a0a",
+    "name": "Event Description/Summary",
+    "enabled": true,
+    "order": 2,
+    "placeholder": "What happened, Where did the event occur, Who are the parties involved, How the event happened",
+    "required": true,
+    "type": "textarea",
+    "conditional": {
+      "status": false
+    },
+    "tooltip": {
+      "status": true,
+      "message": "Please enter background, issue description, root cause, high level financial and customer/client impact and discovery source"
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col gap-4'>
+      <h1>GRC Schema Generator</h1>
+      <div className='flex flex-col gap-3'>
+        <h2>Form Field schema generator</h2>
+        <input type='text' value={formId} onChange={(e) => setFormId(e.target.value)} placeholder='Form ID' />
+        <input type='text' value={moduleId} onChange={(e) => setModuleId(e.target.value)} placeholder='Module ID' />
+        <input type='text' value={fieldName} onChange={(e) => setFieldName(e.target.value)} placeholder='Field name' />
+        <input type='text' value={placeholder} onChange={(e) => setPlaceholder(e.target.value)} placeholder='Placeholder' />
+        <input type='text' value={tooltip} onChange={(e) => setTooltip(e.target.value)} placeholder='Tooltip' />
+        <textarea className="w-full h-40" value={JSON.stringify(genSchema)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div >
   )
 }
 
-export default App
+export default App;
